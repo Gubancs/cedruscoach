6e5eba04448208ebf6a60a889be4e432294a862c
Kökény Gábor <kokeny.gabor@smb.hu>
2024-12-07 18:35:33 +0100

    Initial Commit

---
+++ cedruscoachTests/cedruscoachTests.swift
@@ -0,0 +1,17 @@
+//
+//  cedruscoachTests.swift
+//  cedruscoachTests
+//
+//  Created by Gabor Kokeny on 07/12/2024.
+//
+
+import Testing
+@testable import cedruscoach
+
+struct cedruscoachTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
